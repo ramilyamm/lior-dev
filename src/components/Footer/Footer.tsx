96d5b670da0e31eb02871155341bd88bcb29fcd3
BayaDev-IT <bkut.004@gmail.com>
2024-03-19 15:33:19 +0300

    Done form and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
 import s from './Footer.module.scss'
 import logo from '../../Assets/image/Ramilya/logo.png'
 import tg from '../../Assets/image/Ramilya/telegram.png'
@@ -6,35 +6,42 @@ import inst from '../../Assets/image/Ramilya/instagram.png'
 import whatsapp from '../../Assets/image/Ramilya/whatsapp.png'
 
 const Footer: FC = () => {
-    return (
-        <footer className={s.footer}>
-            <div className='container'>
-                <div className={s.wrapper}>
-                    <div>
-                        <img src={logo} alt="logo" />
-                    </div>
-                    <h3 className={s.description}>Мы рады предложить индивидуальный подход
-                        к каждому проекту, гарантируя внимание
-                        к деталям и стремление к совершенству во всём, что мы делаем.</h3>
-                    <div>
-                        <h2 className={s.social_block}>Свяжитесь с нами</h2>
-                        <div className={s.image_wrapper}>
-                            <a href="https:t.me/">
-                                <img src={tg} alt="tg" />
-                            </a>
-                            <a href="">
-                                <img src={inst} alt="inst" />
-                            </a>
-                            <a href="">
-                                <img src={whatsapp} alt="whatsapp" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <h2 className={s.footer_end}>Все права зашищены 2024 г.</h2>
-            </div>
-        </footer>
-    );
-};
+	return (
+		<footer className={s.footer}>
+			<div className='container'>
+				<div className={s.content}>
+					<div className={s.wrapper}>
+						<div>
+							<img src={logo} alt='logo' />
+						</div>
+						<h3 className={s.description}>
+							Мы рады предложить индивидуальный подход к каждому проекту,
+							гарантируя внимание к деталям и стремление к совершенству во всём,
+							что мы делаем.
+						</h3>
+						<div>
+							<h2 className={s.social_block}>Свяжитесь с нами</h2>
+							<div className={s.image_wrapper}>
+								<a href='https:t.me/'>
+									<img src={tg} alt='tg' />
+								</a>
+								<a href=''>
+									<img src={inst} alt='inst' />
+								</a>
+								<a href=''>
+									<img src={whatsapp} alt='whatsapp' />
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className={s.bottom_info}>
+						<div className={s.line}></div>
+						<h2 className={s.footer_end}>Все права зашищены 2024 г.</h2>
+					</div>
+				</div>
+			</div>
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
